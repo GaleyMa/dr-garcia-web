c4755c819f6c09b1f1fc6e021602f3058925e1eb
MayraGG <galeyma7@proton.me>
2026-08-08 17:59:07 -0700

    Se agrego topbar con infromacion de contacto

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,6 +5,16 @@ import styles from './Layout.module.css'
 function Layout() {
     return (
         <>
+            <div className={styles.topbar}>
+                <div className={styles.topbarContenido}>
+                    <a href="mailto:dr.edwin.cirugia@gmail.com" className={styles.topbarItem}>
+                        dr.edwin.cirugia@gmail.com
+                    </a>
+                    <span className={styles.topbarItem}>
+                        Torre Médica Otay, Tijuana
+                    </span>
+                </div>
+            </div>
             <nav className={styles.nav}>
                 <Link to="/" className={styles.logo}>
                     <img src="/logo-barra-principal.png" alt="Dr. Edwin García" />
